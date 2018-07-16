e4c9835557962fa1e380f0e00d3dc6f6635bb462
Nafmo2 <nafmo17@gmail.com>
2018-07-16 13:22:34 +0900

    170 minute after

--- Head.h
+++ Head.h
@@ -2,9 +2,47 @@
 # include <Siv3D.hpp>
 
 class Player{
+public:
+	double x,y,r;
+	double a,dy;
+	int mode;
+	bool alive;
+	double GetX(){ return x; }
+	double GetY(){ return y; }
+	double GetR(){ return r; }
+	bool Alive(){ return alive; }
+	void Dead(){ alive = false; }
+	void init(){
+		x = 100;
+		y = 400;
+		dy = 0.65;
+		a = 0;
+		alive = true;
+	}
+	bool Phy(bool f){
+		if(f){
+			y += a;
+			a += dy;
+			if(y >= 400){
+				y = 400,a = -12;
+				return false;
+			}
+		}
+		else{
 
+		}
+		return true;
+	}
+	Player(){init();}
 };
 
 class Enemy{
-
+	double x,y;
+	double diff,difx;
+	double GetX(){ return x; }
+	double GetY(){ return y; }
+	void init(){
+		x = 100;
+		y = 400;
+	}
 };
--- Main.cpp
+++ Main.cpp
@@ -6,53 +6,63 @@ void Main()
 	const Font font(20);
 	double y=400,a=0,dy=0.65,ex=0,ey;
 	double diff = 10,difx=10;
-	int Time = 0,em=0;
-	bool mf = false,ef = false,dead = false;
+	int Time = 0,em=0,extra=0;
+	bool mf = false,ef = false;
 	Circle PC(100,y,20),EC(ex,360,50);
 	Rect ER(ex,380,30,100);
+	Player P;
+	//Graphics::SetBackground(Color(L"#7f7f7f"));
+	auto Ene = Color(L"#EE873D");
 	while (System::Update())
 	{
-		if(!Input::KeyR.pressed)font(dead).draw();
-		else{ font(L"Reset Dead").draw(200,210,Palette::White,1.0); dead = false; }
-		if(dead){
-			font(L"Your Score is ",Time).drawCenter(240);
+		if(!Input::KeyR.pressed)font(P.Alive()).draw();
+		else{
+			font(L"Reset Dead").draw(200,210,Palette::White,1.0); P.init(); 
+			Time = 0; diff = 10,difx = 10,extra=0;
+			mf = false,ef = false;
+		}
+		if(!P.Alive()){
+			font(L"Your Score is ",Time+extra).drawCenter(240);
 			continue;
 		}
-	//	font(L"ようこそ、Siv3D の世界へ！").draw();
 		if(!Input::KeyDown.pressed || mf){
-			PC.y = y,PC.r = 20;
+			PC.y = P.GetY(),PC.r = 20;
 		}
 		else{
-			PC.y=y+25,PC.r=10;
+			PC.y=P.GetY()+25,PC.r=10;
 		} 
-		PC.draw(Palette::Yellowgreen);
-		if(mf){
-			y += a;
-			a += dy;
-			if(y > 400)y = 400,mf = false;
-		} else if(Input::KeyUp.pressed && !Input::KeyDown.pressed){
+		PC.draw(Color(L"#FFFFFF"));
+		if(Input::KeyUp.pressed && !Input::KeyDown.pressed && !mf){
 			mf = true;
-			a = -12;
 		}
+		if(!P.Phy(mf))mf = false;
 		if(ef){
 			if(em == 0){
 				EC.x = ex;
-				EC.draw(Palette::Red);
-				if(EC.intersects(PC)){ dead = true; }
+				EC.draw(Ene);
+				if(EC.intersects(PC)){ P.Dead(); }
 			}
 			else{
 				ER.x = ex;
-				ER.draw(Palette::Red);
-				if(ER.intersects(PC)){ dead = true; }
+				ER.draw(em == 1?Ene:Palette::Aqua);
+				if(20<ex - P.GetX()&&ex - P.GetX()<20+50 && Input::KeyRight.clicked && !mf&& em==2){
+					extra += 1000;
+					ef = false; 
+				}
+				if(ER.intersects(PC)){ P.Dead(); }
 			}
 			ex -= difx;
 			
 			if(ex < 0)ef = false;
 		}
+		Rect(P.GetX(),0,50,480).drawFrame();
+		Line(P.GetX(),P.GetY(),ex,P.GetY()).draw();
+		font(ex - P.GetX()).draw(400,20);
 		if(!ef){
 			ef = true;
 			ex = 640,ey = 400;
-			em=Random<int>(0,1);
+			em = Random<int>(0,1);
+			if(em == 1)em+=Random<int>(0,1);
 			difx=Random<int>(Max<int>(10,diff-6),diff)/2.0;
 		}
 		//Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
@@ -60,6 +70,6 @@ void Main()
 		if(!(Time % 120) && diff < 32){
 			diff += 2; 
 		}
-		font(L"D:",diff/2,L" Dx:",difx,L" T:",Time).draw(0,50,Palette::White,1.0);
+		font(L"D:",diff,L" Dx:",difx,L" S:",Time+extra).draw(0,50,Palette::White,1.0);
 	}
 }
